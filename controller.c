b594981cd00373057603f76eaedcc5fa3dc8ebef
a104434 <a104434@alunos.uminho.pt>
2026-04-11 16:17:25 +0100

    Acrescentei outra politica que executa os programas com menos execucoes ate ao momento

--- controller.c
+++ controller.c
@@ -5,7 +5,6 @@ int main(int argc, char *argv[]) {
         printf("ERROR: insufficient arguments ...\n");
         return -1;
     }
-    
 
     int parallel = atoi(argv[1]);
     char *policy = argv[2];
@@ -19,6 +18,8 @@ int main(int argc, char *argv[]) {
 
     int shutdown_requested = 0;
     pid_t shutdown_pid = -1;
+
+    int user_executions[MAX_RUNNERS] = {0}; // index is user_id, value is number of executions ; index 0 is total executions
 
 
     // create FIFO 
@@ -83,12 +84,19 @@ int main(int argc, char *argv[]) {
                 gettimeofday(&end_time, NULL);
                 for (int i = 0; i < number_of_running_commands; i++) {
                     if (running_commands[i].pid == request.pid) {
+
+                        struct timeval start = running_commands[i].start_time;
+
+                        int user_id = running_commands[i].user_id;
+                        user_executions[user_id]++;
+                        user_executions[0]++; // total executions
+
                         for (int j = i; j < number_of_running_commands - 1; j++) {
                             running_commands[j] = running_commands[j + 1];  // shift the remaining commands to fill the gap
                         }
                         number_of_running_commands--;
 
-                        double duration = (end_time.tv_sec - running_commands[i].start_time.tv_sec) + (end_time.tv_usec - running_commands[i].start_time.tv_usec) / 1000000.0;
+                        double duration = (end_time.tv_sec - start.tv_sec) + (end_time.tv_usec - start.tv_usec) / 1000000.0;
                         int log_fd = open("../tmp/logs.txt", O_WRONLY | O_CREAT | O_APPEND, 0666);
                         if (log_fd < 0) {
                             printf("ERROR: failed to open/create log.txt\n");
@@ -115,7 +123,6 @@ int main(int argc, char *argv[]) {
 
 
                     pid_t pid = fork();
-                    // 
                     if (pid < 0) {
                         printf("ERROR: failed to fork process\n");
                         return -1;
@@ -136,6 +143,42 @@ int main(int argc, char *argv[]) {
                         waitpid(pid, NULL, WNOHANG);
                     }
 
+                }
+            // Least Executed First (LEF)
+            } else if (strcmp(policy, "lef") == 0) {
+                while (number_of_running_commands < parallel && number_of_scheduled_commands > 0) {
+
+                    int idx = pick_next(scheduled_commands, number_of_scheduled_commands, user_executions);
+                    Message next = scheduled_commands[idx];
+
+                    for (int i = idx; i < number_of_scheduled_commands - 1; i++) {
+                        scheduled_commands[i] = scheduled_commands[i + 1];  // shift the remaining commands to fill the gap
+                    }
+
+                    number_of_scheduled_commands--;
+                    running_commands[number_of_running_commands++] = next;
+
+
+                    pid_t pid = fork(); 
+                    if (pid < 0) {
+                        printf("ERROR: failed to fork process\n");
+                        return -1;
+                    }
+
+                    if (pid == 0) {
+                        char fifo_controller_to_runner[256];
+                        sprintf(fifo_controller_to_runner, "fifo_controller_to_runner_%d", next.pid);
+                        int fd_confirmation = open(fifo_controller_to_runner, O_WRONLY);
+                        if (fd_confirmation < 0) {
+                            printf("ERROR: failed to open %s\n", fifo_controller_to_runner);
+                            _exit(-1);
+                        }
+                        write(fd_confirmation, &next.pid, sizeof(pid_t)); // send the command_id as confirmation
+                        close(fd_confirmation);
+                        _exit(0); 
+                    } else {
+                        waitpid(pid, NULL, WNOHANG);
+                    }
                 }
             }
         }
--- utils.h
+++ utils.h
@@ -10,6 +10,8 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 
+#define MAX_RUNNERS 10
+
 typedef struct {
     int type; // operation types: 1 for execute, 2 for read, 3 for exit, 4 for status
     int user_id;
@@ -18,5 +20,15 @@ typedef struct {
     char command[32][256]; // buffer to store the command and its arguments
     struct timeval start_time; 
 } Message;
+
+int pick_next(Message *scheduled, int n_scheduled, int *user_executions) {
+    int best = 0;
+    for (int i = 1; i < n_scheduled; i++) {
+        if (user_executions[scheduled[i].user_id] < user_executions[scheduled[best].user_id]) {
+            best = i;
+        }
+    }
+    return best;
+}
 
 #endif
